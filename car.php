a93fbf36abe30c069a576ad60ba55784c5894ae4
Jason Bethel & Chris Swan <student@epicodus.com>
2015-08-03 11:26:24 -0700

    added setters & private, needs gettersa added

--- car.php
+++ car.php
@@ -2,36 +2,53 @@
 class Car
 {
     public $make_model;
-    public $price;
+    private $price;
     public $miles;
+
+    function worthBuying($max_price)
+    {
+      return $this->price < ($max_price + 100);
+    }
+
+    function __construct($make_model, $price, $miles)
+    {
+      $this->make_model = $make_model;
+      $this->price = $price;
+      $this->miles = $miles;
+    }
+
+    function setMake_Model($new_make_model)
+    {
+      $this->make_model = $make_model;
+    }
+
+    function setPrice($new_price)
+    {
+      $float_price = (float) $new_price;
+      if ($float_price != 0) {
+          $formatted_price = number_format($float_price, 2);
+          $this->price = $formatted_price;
+      }
+    }
+
+    function setMiles($new_miles)
+    {
+      $this->miles = $new_miles;
+    }
 }
 
+$first_car = new Car("2014 Porsche 911", 7864, 114991);
+$second_car = new Car("2011 Ford F450", 14000, 55995);
+$third_car = new Car("2013 Lexus RX 350", 20000, 44700);
+$fourth_car = new Car("Mercedes Benz CLS550", 37979, 39900);
 
-$porsche = new Car();
-$porsche->make_model = "2014 Porsche 911";
-$porsche->price = 114991;
-$porsche->miles = 7864;
 
-$ford = new Car();
-$ford->make_model = "2011 Ford F450";
-$ford->price = 55995;
-$ford->miles = 14241;
-
-$lexus = new Car();
-$lexus->make_model = "2013 Lexus RX 350";
-$lexus->price = 44700;
-$lexus->miles = 20000;
-
-$mercedes = new Car();
-$mercedes->make_model = "Mercedes Benz CLS550";
-$mercedes->price = 39900;
-$mercedes->miles = 37979;
-
-$cars = array($porsche, $ford, $lexus, $mercedes);
+$cars = array($first_car, $second_car, $third_car, $fourth_car);
 
 $cars_matching_search = array();
+
 foreach ($cars as $car) {
-    if ($car->price < $_GET["price"]) {
+    if ($car->worthBuying($_GET["price"])) {
         array_push($cars_matching_search, $car);
     }
 }
